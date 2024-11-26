e9769d27b03364fd02852d22c13314f039f79c10
KevinLin <dolphin0317@gmail.com>
2024-11-26 13:26:25 +0800

    Create dynamicload-filelist.js

---
+++ dynamicload-filelist.js
@@ -0,0 +1,15 @@
+ // 使用 fetch 動態加載 file-list.html
+<script type="javascript">
+  let file_name='file-list.txt'
+
+  fetch(file_name)
+    .then(response => response.text())
+    .then(data => {
+        document.querySelector('.file-list ul').innerHTML = data;
+    })
+    .catch(error => {
+        console.error('載入檔案列表時發生錯誤:', error);
+        document.querySelector('.file-list ul').innerHTML = '<li>無法載入檔案列表。</li>';
+    });
+
+</script>
