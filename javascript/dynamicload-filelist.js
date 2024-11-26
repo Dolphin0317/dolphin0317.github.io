8d338dd648a75cfb13ab2e895bbc6ab6e7226f48
KevinLin <dolphin0317@gmail.com>
2024-11-26 15:45:46 +0800

    Update dynamicload-filelist.js

--- javascript/dynamicload-filelist.js
+++ javascript/dynamicload-filelist.js
@@ -1,12 +1,16 @@
+const fileList = document.getElementById('file-list');
+
 // 使用 fetch 動態加載 file-list.html
-let file_name='file-list.txt'
+const file_name='file-list.txt'
 
 fetch(file_name)
   .then(response => response.text())
   .then(data => {
       // 將檔案清單分割成陣列
       const files = data.trim().split('\n');
-      const fileList = document.getElementById('file-list');
+    
+      // 清空預設的 "檔案列表正在載入中..."
+      fileList.innerHTML = '';
     
       // 生成列表項目
       files.forEach(file => {
@@ -20,5 +24,5 @@ fetch(file_name)
   })
   .catch(error => {
       console.error('載入檔案列表時發生錯誤:', error);
-      document.querySelector('.file-list ul').innerHTML = '<li>無法載入檔案列表。</li>';
+      fileList.innerHTML = '<li>無法載入檔案列表</li>';
   });
