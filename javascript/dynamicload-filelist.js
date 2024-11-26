a37d72334e298c4da3a36b67c05f9337ca07e5ee
KevinLin <dolphin0317@gmail.com>
2024-11-26 16:10:10 +0800

    Update dynamicload-filelist.js

--- javascript/dynamicload-filelist.js
+++ javascript/dynamicload-filelist.js
@@ -7,6 +7,7 @@ const file_name='file-list.html'
 fetch(file_name)
   .then(response => response.text())
   .then(data => {
+      document.querySelector('.file-list2 ul').innerHTML = data;
       // 將檔案清單分割成陣列
       const files = data.trim().split('\n');
     
